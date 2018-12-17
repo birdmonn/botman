97d998ea9d736c128f4c3149b0c5c4fcaf5afb51
birdmonn <girapat.bird@gmail.com>
2018-12-17 17:08:23 +0700

    add

--- src/main/java/com/sapo/botman/BotmanApplication.java
+++ src/main/java/com/sapo/botman/BotmanApplication.java
@@ -1,13 +1,12 @@
 package com.sapo.botman;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import com.linecorp.bot.model.event.MessageEvent;
 import com.linecorp.bot.model.event.message.TextMessageContent;
 import com.linecorp.bot.model.message.Message;
 import com.linecorp.bot.model.message.TextMessage;
 import com.linecorp.bot.spring.boot.annotation.EventMapping;
-import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BotmanApplication {
