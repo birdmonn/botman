b48b92e243c5058815b7e74d410795a275506571
birdmonn <girapat.bird@gmail.com>
2018-12-17 16:41:24 +0700

    first commit

---
+++ src/main/java/com/sapo/botman/BotmanApplication.java
@@ -0,0 +1,26 @@
+package com.sapo.botman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.linecorp.bot.model.event.MessageEvent;
+import com.linecorp.bot.model.event.message.TextMessageContent;
+import com.linecorp.bot.model.message.Message;
+import com.linecorp.bot.model.message.TextMessage;
+import com.linecorp.bot.spring.boot.annotation.EventMapping;
+import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
+
+@SpringBootApplication
+public class BotmanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BotmanApplication.class, args);
+	}
+
+	@EventMapping
+	public Message handleTextMessage(MessageEvent<TextMessageContent> e) {
+		System.out.println("event: " + e);
+		TextMessageContent message = e.getMessage();
+		return new TextMessage(message.getText());
+	}
+}
+
---
+++ src/test/java/com/sapo/botman/BotmanApplicationTests.java
@@ -0,0 +1,17 @@
+package com.sapo.botman;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BotmanApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
