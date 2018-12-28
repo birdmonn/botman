1e748024ebfce2f02bc4faf6cf14a17a56dddaef
birdmonn <girapat.bird@gmail.com>
2018-12-28 10:23:58 +0700

    new img quest

--- src/main/java/com/sapo/botman/BotmanApplication.java
+++ src/main/java/com/sapo/botman/BotmanApplication.java
@@ -2,22 +2,13 @@ package com.sapo.botman;
 
 import com.sapo.botman.model.QuestPokemonGo;
 import com.sapo.botman.service.QuestPokemonGoService;
-import com.sapo.botman.storage.StorageProperties;
-import com.sapo.botman.storage.StorageService;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-
 @SpringBootApplication
-@EnableConfigurationProperties(StorageProperties.class)
 public class BotmanApplication {
 	@Autowired
 	private QuestPokemonGoService questPokemonGoService;
@@ -32,13 +23,6 @@ public class BotmanApplication {
 			questPokemonGoService.create(new QuestPokemonGo());
 		}
 		return null;
-	}
-
-	@Bean
-	CommandLineRunner init(StorageService storageService) {
-		return (args) -> {
-			storageService.init();
-		};
 	}
 }
 
