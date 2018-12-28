16dff1fe45d4c5c9f01e4f482a900a2fd18005e2
birdmonn <girapat.bird@gmail.com>
2018-12-28 10:44:45 +0700

    new command quest

--- src/main/java/com/sapo/botman/controller/MainController.java
+++ src/main/java/com/sapo/botman/controller/MainController.java
@@ -1,61 +1,30 @@
 package com.sapo.botman.controller;
 
-import com.google.common.io.ByteStreams;
 import com.linecorp.bot.client.LineMessagingClient;
-import com.linecorp.bot.client.MessageContentResponse;
-import com.linecorp.bot.model.ReplyMessage;
 import com.linecorp.bot.model.event.Event;
 import com.linecorp.bot.model.event.MessageEvent;
-import com.linecorp.bot.model.event.message.ImageMessageContent;
-import com.linecorp.bot.model.event.message.StickerMessageContent;
 import com.linecorp.bot.model.event.message.TextMessageContent;
 import com.linecorp.bot.model.message.ImageMessage;
-import com.linecorp.bot.model.message.Message;
-import com.linecorp.bot.model.message.StickerMessage;
 import com.linecorp.bot.model.message.TextMessage;
-import com.linecorp.bot.model.response.BotApiResponse;
 import com.linecorp.bot.spring.boot.annotation.EventMapping;
 import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
-import com.sapo.botman.config.ConfigGroup;
 import com.sapo.botman.model.QuestPokemonGo;
 import com.sapo.botman.service.QuestPokemonGoService;
-import com.sapo.botman.storage.StorageProperties;
-import com.sapo.botman.storage.StorageService;
-import org.checkerframework.checker.nullness.qual.NonNull;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import java.io.IOException;
-import java.io.OutputStream;
-import java.io.UncheckedIOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
-import java.time.LocalDateTime;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.UUID;
-import java.util.concurrent.ExecutionException;
 
 @LineMessageHandler
 public class MainController {
 
     private LineMessagingClient lineMessagingClient;
-    private StorageProperties properties;
     private QuestPokemonGoService questPokemonGoService;
-    private StorageService storageService;
 
     @Autowired
-    public MainController(StorageProperties properties,
-                          LineMessagingClient lineMessagingClient,
-                          QuestPokemonGoService questPokemonGoService,
-                          StorageService storageService) {
-        this.properties = properties;
+    public MainController(LineMessagingClient lineMessagingClient,
+                          QuestPokemonGoService questPokemonGoService) {
         this.lineMessagingClient = lineMessagingClient;
         this.questPokemonGoService = questPokemonGoService;
-        this.storageService = storageService;
     }
 
     @EventMapping
@@ -69,16 +38,16 @@ public class MainController {
         handleTextContent(event.getReplyToken(), event, message);
     }
 
-
-    @EventMapping
-    public void handleStickerMessage(MessageEvent<StickerMessageContent> event) {
-        StickerMessageContent message = event.getMessage();
-        if (!event.getSource().getSenderId().equals(ConfigGroup.GROUPID)) {
-            new ReplayController(lineMessagingClient).reply(event.getReplyToken(), new StickerMessage(
-                    message.getPackageId(), message.getStickerId()
-            ));
-        }
-    }
+//
+//    @EventMapping
+//    public void handleStickerMessage(MessageEvent<StickerMessageContent> event) {
+//        StickerMessageContent message = event.getMessage();
+//        if (!event.getSource().getSenderId().equals(ConfigGroup.GROUPID)) {
+//            new ReplayController(lineMessagingClient).reply(event.getReplyToken(), new StickerMessage(
+//                    message.getPackageId(), message.getStickerId()
+//            ));
+//        }
+//    }
 
     private void handleTextContent(String replyToken, Event event, TextMessageContent content) {
         String text = content.getText();
@@ -86,11 +55,13 @@ public class MainController {
             case "Profile":
                 showProfile(replyToken, event);
                 break;
+            case "#quest":
+                showQuestPokemon(replyToken);
+                break;
             default:
                 new ReplayController(lineMessagingClient).replyText(replyToken, text);
         }
     }
-
 
 
     private void showProfile(String replyToken, Event event) {
@@ -111,6 +82,10 @@ public class MainController {
         }
     }
 
+    private void showQuestPokemon(String replyToken) {
+        QuestPokemonGo questPokemonGo = questPokemonGoService.findAll().get(0);
+        new ReplayController(lineMessagingClient).reply(replyToken, new ImageMessage(questPokemonGo.getUrl(), questPokemonGo.getUrl()));
+    }
 //    @EventMapping
 //    public void handleImageMessage(MessageEvent<ImageMessageContent> event) {
 ////        log.info(event.toString());
