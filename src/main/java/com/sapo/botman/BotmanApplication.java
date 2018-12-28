03cf7f069c6aa00bf5ea4b8cc36a4fe20501764e
birdmonn <girapat.bird@gmail.com>
2018-12-28 10:35:52 +0700

    fix error deploy

--- src/main/java/com/sapo/botman/BotmanApplication.java
+++ src/main/java/com/sapo/botman/BotmanApplication.java
@@ -2,13 +2,18 @@ package com.sapo.botman;
 
 import com.sapo.botman.model.QuestPokemonGo;
 import com.sapo.botman.service.QuestPokemonGoService;
+import com.sapo.botman.storage.StorageProperties;
+import com.sapo.botman.storage.StorageService;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
 public class BotmanApplication {
 	@Autowired
 	private QuestPokemonGoService questPokemonGoService;
@@ -23,6 +28,13 @@ public class BotmanApplication {
 			questPokemonGoService.create(new QuestPokemonGo());
 		}
 		return null;
+	}
+
+	@Bean
+	CommandLineRunner init(StorageService storageService) {
+		return (args) -> {
+			storageService.init();
+		};
 	}
 }
 
